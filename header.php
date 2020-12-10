5a5591ba61c2b7d9cc8c76cc4bc0ed4cb63b8d34
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-12-10 15:41:25 -0800

    updated header

--- header.php
+++ header.php
@@ -37,11 +37,11 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<p class="site-title"><?php bloginfo( 'name' ); ?></p>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><?php bloginfo( 'name' ); ?></p>
 				<?php
 			endif;
 			$cyberpunkcoder_description = get_bloginfo( 'description', 'display' );
